69e7f25bb04f7574a8b55ce41332776e6c123a7e
Marcin Badurowicz <m@badurowicz.net>
2020-10-29 19:46:37 +0100

    initial revision

---
+++ Glcv19stats/Glcv19stats.Android/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+
+namespace Glcv19stats.Droid
+{
+    [Activity(Label = "glcv19stats", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [MetaData("com.google.android.glass.VoiceTrigger", Resource = "@xml/voice_trigger")]
+    [IntentFilter(actions: new[] { "android.intent.action.MAIN" }, Categories = new[] { "android.intent.category.LAUNCHER" })]
+    [IntentFilter(actions: new[] { "com.google.android.glass.action.VOICE_TRIGGER" })]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            //TabLayoutResource = Resource.Layout.Tabbar;
+            //ToolbarResource = Resource.Layout.Toolbar;
+
+            AppCompatDelegate.CompatVectorFromResourcesEnabled = true;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public override bool OnKeyUp([GeneratedEnum] Keycode keyCode, KeyEvent e)
+        {
+            return base.OnKeyUp(keyCode, e);
+        }
+    }
+}
---
+++ Glcv19stats/Glcv19stats/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using Xamarin.Forms;
+
+namespace Glcv19stats
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ShowCovidData();
+        }
+
+        public async void ShowCovidData()
+        {
+            activity.IsVisible = true;
+
+            try
+            {
+                var hc = new HttpClient();
+                var result = await hc.GetAsync("https://api.covid19api.com/country/poland/status/confirmed");
+
+                var covidData = JsonSerializer.Deserialize<List<CovidData>>(await result.Content.ReadAsStringAsync());
+
+                var last = covidData.Last();
+
+                cases.Text = last.Cases.ToString();
+                cases2.Text = (last.Cases - covidData[covidData.Count - 2].Cases).ToString();
+                date.Text = DateTime.Parse(last.Date).Humanize();
+
+                activity.IsVisible = false;
+                data.IsVisible = true;
+            }
+            catch (Exception e)
+            {
+                data.IsVisible = true;
+
+                cases.Text = e.ToString();
+                cases2.Text = "";
+                date.Text = "";
+            }
+        }
+    }
+
+    internal class CovidData
+    {
+        public int Cases { get; set; }
+        public string Date { get; set; }
+    }
+}
